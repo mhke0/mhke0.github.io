49e097fe2f09bbf0660bbdf80f0bbb686fc77dc0
mhke0 <moritzhacke@gmail.com>
2024-08-28 14:51:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -133,8 +133,6 @@ function createResponsiveChart(chartId, traces, layout, config = {}) {
             layout.legend.font = layout.legend.font || {};
             layout.legend.font.size = baseSize;
         }
-            layout.legend.font = layout.legend.font || {};
-            layout.legend.font.size = baseSize * 0.8;
 
     }
     

08854d4693b73128c06172df5c9ced2c5a2c0b16
mhke0 <moritzhacke@gmail.com>
2024-08-29 18:45:24 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# URLs for data sources
+CYCLIST_URL = "https://www.velogames.com/spain/2024/riders.php"
+LEAGUE_SCORES_URL = "https://www.velogames.com/spain/2024/leaguescores.php?league=764413216"
+TWITTER_LEAGUE_URL = "https://www.velogames.com/spain/2024/leaguescores.php?league=23161631"
+
+# File paths
+OUTPUT_FILE = "cyclist-data.json"
+
+# Constants
+TEAM_SIZE = 9
+MAX_TEAM_COST = 100
+MIN_SPRINTERS = 1
+MIN_ALL_ROUNDERS = 2
+MIN_CLIMBERS = 2
+MIN_UNCLASSED = 3
+HISTORY_DAYS = 30
